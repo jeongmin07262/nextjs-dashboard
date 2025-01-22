1b9509c731a66391a19fe34339b0b48fb7a1137a
jeongmin07262 <dlwjdals0726@gmail.com>
2025-01-22 15:45:18 +0900

    8. Streaming

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,7 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 import { Revenue } from '@/app/lib/definitions';
 
 //  This component is representational only.
@@ -9,18 +10,17 @@ import { Revenue } from '@/app/lib/definitions';
 //  https:www.chartjs.org/
 //  https:airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+
+
+export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
   const chartHeight = 350;
-
-   const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
-   if (!revenue || revenue.length === 0) {
-     return <p className="mt-4 text-gray-400">No data available.</p>;
-   }
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+ 
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
 
   return (
     <div className="w-full md:col-span-4">
